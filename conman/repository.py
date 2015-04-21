0ec2ba184d277244ad86be7df3a598c2f93767b2
Mogria <m0gr14@gmail.com>
2015-04-21 22:39:34 +0200

    added methods to add files and links to modules

--- conman/repository.py
+++ conman/repository.py
@@ -1,9 +1,15 @@
 from git import Repo, Actor
 import os
+import shutil
 
 class Repository(object):
 
+    LINK_FILES_NAME = "conman-link-files"
+    MODULES_DIR = "modules"
+    USE_MODULES_FILE = "use-modules"
+
     def __init__(self, directory, machine):
+        """Create an instance by using a local repository"""
         self.directory = directory
         self.machine = machine
         self.branch = 'machine-' + machine
@@ -14,41 +20,126 @@ class Repository(object):
         self.__conmanify()
 
     def clone(remote_url, directory, machine):
+        """Create an instance by using a remote repository"""
         Repo.clone_from(remote_url, directory)
         return Repository(directory, machine)
 
     def get_modules(self):
-        trees = (self.git_repo.tree() / 'modules').trees
+        """Returns a list of all the module names"""
+        trees = (self.git_repo.tree() / self.MODULES_DIR).trees
         return [item.name for item in trees]
 
     def add_module(self, name):
+        """Adds a module"""
         if self.is_module(name):
             raise NameError('module named `' + name + '` already exists')
         module_dir = self.get_module_directory(name)
-        Repository.__createdir(module_dir)
+        Repository.__createfile(os.path.join(module_dir, self.LINK_FILES_NAME))
         self.__commit('add module directory for `' + name + '`', [module_dir])
 
     def is_module(self, name):
+        """Returns a boolean wheter a module exists"""
         return name in self.get_modules()
 
+    def __check_have_module(self, module):
+        """Raises a NameError if the given module doesn't exist"""
+        if not self.is_module(module):
+            raise NameError('no module named `' + module + '`')
+
+    def __check_have_file_in_module(self, module, filename):
+        """Raises a NameError if the given module doesn't contain the given file"""
+        if not filename in self.get_files_in_module(module):
+            raise NameError('no file with the given name (`' + filename + '`) in module `' + module + '`')
+
+    def add_file_to_module(self, module, filename, filepath):
+        """Adds a file to a module.
+
+        filename is relative to the module folder.
+        filepath a file which gets copied into the module.
+        """ 
+        self.__check_have_module(module)
+        module_dir = self.get_module_directory(module)
+        dest_file = os.path.join(module_dir, filename)
+        Repository.__createfile(dest_file)
+        shutil.copyfile(filepath, dest_file)
+        if filename in self.get_files_in_module(module):
+            self.update_file_in_repository(module, filename)
+        self.__commit('add file `' + filename + '` in module `' + module +'`', [dest_file])
+
+    def update_file_in_repository(self, module, filename):
+        self.__check_have_module(module)
+        self.__check_have_file_in_module(module, filename)
+        self.__commit('update file `' + filename + '` in module `' + module +'`', [self.get_filepath(module, filename)])
+
+    def get_files_in_module(self, module):
+        """lists all the files in a module which are already committed"""
+        self.__check_have_module(module)
+        module_tree = (self.git_repo.tree() / self.MODULES_DIR / module)
+        files = []
+        for item in module_tree.traverse():
+            if item.type == 'blob':
+                cut_module_dir_at = len(module_tree.path) + 1
+                filename = item.path[cut_module_dir_at:]
+                if filename != self.LINK_FILES_NAME:
+                    files.append(filename)
+
+        return files
+
+    def get_links_in_module(self, module):
+        self.__check_have_module(module)
+        module_dir = self.get_module_directory(module)
+        link_files = os.path.join(module_dir, self.LINK_FILES_NAME)
+        with open(link_files, 'r') as f:
+            data = f.read()
+            return dict([line.split(':') for line in data.split()])
+
+    def get_links(self):
+        result = {}
+        for module in self.get_modules():
+            links = self.get_links_in_module(module)
+            links = dict([[key, os.path.join(self.MODULES_DIR, module, links[key])] for key in links])
+            result.update(links)
+        return result
+
+    def link_file_from_module(self, module, filename, linkpath):
+        """Adds an entry to the modules link file."""
+        self.__check_have_module(module)
+        self.__check_have_file_in_module(module, filename)
+
+        if linkpath in self.get_links():
+            raise NameError('some other file already links to the exact same location')
+
+        module_dir = self.get_module_directory(module)
+        link_files = os.path.join(module_dir, self.LINK_FILES_NAME)
+        with open(link_files, 'a') as f:
+            f.write(linkpath + ':' + filename + '\n')
+        self.__commit('add link `' + linkpath + '` to file `' + filename + '` in module `' + module + '`', [link_files])
+        
     def get_module_directory(self, name):
-        return os.path.join(self.directory, 'modules', name)
+        """Returns the full directory to a module"""
+        return os.path.join(self.directory, self.MODULES_DIR, name)
+
+    def get_filepath(self, module, filename):
+        """Returns the path to a file by its module and filename"""
+        module_dir = self.get_module_directory(module)
+        return os.path.join(module_dir, filename)
 
     def __create_modules_directory(self):
-        modules_directory = os.path.join(self.directory, 'modules')
+        modules_directory = os.path.join(self.directory, self.MODULES_DIR)
         Repository.__createdir(modules_directory)
         self.__commit('initialized conman `modules/` directory', [modules_directory])
 
     def __create_machine_file(self):
-        use_modules_file = os.path.join(self.directory, 'use-modules')
+        use_modules_file = os.path.join(self.directory, self.USE_MODULES_FILE)
         Repository.__createfile(use_modules_file)
         self.__commit('create use-module file for machine `' + self.machine + '`', [use_modules_file])
 
     def __conmanify(self):
+        """Make sure all the needed directories and files are in the repository"""
         if len(self.git_repo.heads) == 0:
             self.__create_modules_directory()
 
-        if not 'modules' in self.git_repo.tree().trees:
+        if not self.MODULES_DIR in self.git_repo.tree().trees:
             self.__create_modules_directory();
 
         if not self.branch in self.git_repo.heads:
@@ -57,10 +148,11 @@ class Repository(object):
         self.git_repo.head.reference = self.git_repo.heads[self.branch]
         self.git_repo.head.reset(index=True, working_tree=True)
 
-        if not 'use-modules' in self.git_repo.tree().blobs:
+        if not self.USE_MODULES_FILE in self.git_repo.tree().blobs:
             self.__create_machine_file()
 
     def __commit(self, message, files):
+        """Add files and create a commit"""
         index = self.git_repo.index
         index.add(files)
         index.commit(message, author=self.actor, committer=self.actor)
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -2,6 +2,7 @@ from conman.repository import Repository
 import unittest
 from subprocess import call
 import tempfile
+import shutil
 import os
 
 class TestRepositoryConstruction(unittest.TestCase):
@@ -9,9 +10,18 @@ class TestRepositoryConstruction(unittest.TestCase):
         self.empty_origin_repo = tempfile.mkdtemp();
         call(['git', 'init', '--quiet', self.empty_origin_repo])
         self.empty_repo  = tempfile.mkdtemp();
+        _, self.file1 = tempfile.mkstemp()
+        with open(self.file1, 'w') as f:
+            f.write('some sample configuration\nfile')
+        _, self.file2 = tempfile.mkstemp()
+        with open(self.file1, 'w') as f:
+            f.write('another file with some contents and\nstuff. Yeah ...\nok?')
 
     def tearDown(self):
-        call(['rm', '-rf', self.empty_origin_repo, self.empty_repo])
+        shutil.rmtree(self.empty_origin_repo)
+        shutil.rmtree(self.empty_repo)
+        os.remove(self.file1)
+        os.remove(self.file2)
 
     def test_construct(self):
         repo = Repository(self.empty_origin_repo, "some-machine")
@@ -55,4 +65,56 @@ class TestRepositoryConstruction(unittest.TestCase):
         assert repo.is_module('module-y')
         assert repo.is_module('module-z')
 
+    def test_check_have_module(self):
+        repo = Repository(self.empty_origin_repo, "some-machine")
+        with self.assertRaises(NameError):
+            repo._Repository__check_have_module('inexistent-module')
 
+    def test_check_have_file_in_module(self):
+        repo = Repository(self.empty_origin_repo, "some-machine")
+        repo.add_module('module-z')
+        with self.assertRaises(NameError):
+            repo._Repository__check_have_file_in_module('module-z', 'inexistent-module')
+
+        with self.assertRaises(NameError):
+            repo._Repository__check_have_file_in_module('module-z', 'conman-link-files')
+
+    def test_add_file_to_module(self):
+        repo = Repository(self.empty_origin_repo, "some-machine")
+        repo.add_module('module-x')
+        assert set(repo.get_files_in_module('module-x')) == set([])
+        repo.add_file_to_module('module-x', 'cfg1', self.file1)
+        assert set(repo.get_files_in_module('module-x')) == set(['cfg1'])
+        repo.add_file_to_module('module-x', 'subdir1/d/cfg2', self.file2)
+        assert set(repo.get_files_in_module('module-x')) == set(['cfg1', 'subdir1/d/cfg2'])
+
+    def test_link_file_from_module(self):
+        repo = Repository(self.empty_origin_repo, "some-machine")
+        repo.add_module('module-x')
+        repo.add_file_to_module('module-x', 'file1', self.file1)
+        repo.add_file_to_module('module-x', 'd/file2', self.file2)
+
+        with self.assertRaises(NameError):
+            repo.link_file_from_module('module-x', 'inexistent-file', 'some-location')
+
+        repo.link_file_from_module('module-x', 'file1', 'some-location')
+        assert repo.get_links_in_module('module-x')['some-location'] == 'file1'
+
+        with self.assertRaises(NameError):
+            repo.link_file_from_module('module-x', 'd/file2', 'some-location')
+
+        repo.link_file_from_module('module-x', 'd/file2', 'some-other-location')
+        assert repo.get_links_in_module('module-x')['some-other-location'] == 'd/file2'
+
+    def test_get_links(self):
+        repo = Repository(self.empty_origin_repo, "some-machine")
+        repo.add_module('module-x')
+        repo.add_module('module-y')
+        repo.add_file_to_module('module-x', 'file1', self.file1)
+        repo.add_file_to_module('module-y', 'd/file2', self.file2)
+
+        assert repo.get_links() == {}
+        repo.link_file_from_module('module-x', 'file1', 'some-location')
+        assert repo.get_links()['some-location'] == 'modules/module-x/file1'
+        repo.link_file_from_module('module-y', 'd/file2', 'some-other-location')
+        assert repo.get_links()['some-other-location'] == 'modules/module-y/d/file2'
